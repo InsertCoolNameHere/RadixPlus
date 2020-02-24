b25edc3038bc09015e55280052110cc594ee699a
Saptashwa Mitra <rikimitra779@gmail.com>
2020-02-23 23:50:40 -0700

    Feb23

--- webserver/columbus-master/columbus/settings.py
+++
@@ -1,8 +0,0 @@
-import socket
-
-if socket.gethostname() == 'Jo_SpectreX':
-    from columbus.dev_settings import *
-elif socket.gethostname() == 'columbus-dev-server':
-    from columbus.test_settings import *
-else:
-    from columbus.prod_settings import *
--- webserver/python2.7/site-packages/pyedf/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class ColumbusConfig(AppConfig):
-    name = "pyedf"
-    verbose_name = "Columbus Workflow Engine"
-
-    def ready(self):
-        pass
